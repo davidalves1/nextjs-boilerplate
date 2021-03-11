d0a7d0a449a07e81b5825f3485e8db2d9c22b639
davidalves1 <alves.david@outlook.com>
2021-03-10 23:57:41 -0300

    configure jest and react testing-library

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.spec.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 };
